277045efff5f80e46ae68a2331d7e6d640cc7740
wanjohidev <232580863+wanjohidev@users.noreply.github.com>
2026-07-15 18:41:35 +0300

    Fix: CORS Policy +

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("frontend", policy =>
     {
         policy
-            .WithOrigins("http://127.0.0.1:5500/wwwroot/html/login.html")
+            .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
